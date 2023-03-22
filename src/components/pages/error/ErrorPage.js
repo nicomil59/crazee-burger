2ae2bb2a583a84d71395da8e74158fcad8a4bb9c
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-03-22 15:36:49 +0100

    ✨ F02: LOGIN > add redirection from/to ErrorPage

--- src/components/pages/error/ErrorPage.js
+++ src/components/pages/error/ErrorPage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <div>
+      <h1>ErrorPage</h1>
+      <Link to="/"><button>Retourner vers la page d'accueil</button></Link>
+    </div>
   )
 }
 
