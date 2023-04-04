18e311daa1e8c858dcc7eb4425b952fcadde8048
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-04-04 14:27:51 +0200

    💄 F03 : LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -15,10 +15,24 @@ const LoginPage = () => {
 export default LoginPage;
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
+
+  ::before {
+    content: "";
+    background: url('/images/burger-and-fries-background.jpg') rgba(0, 0, 0, 0.5);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
