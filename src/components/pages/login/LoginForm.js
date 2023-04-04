62b6d9caef711f974881a86162e9a9bb794fbccf
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-04-04 16:32:37 +0200

    💄 F03: LOGIN > LoginForm > input > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
 
 const LoginForm = () => {
   const [inputValue, setInputValue] = useState("");
@@ -21,13 +22,16 @@ const LoginForm = () => {
       <h1>Bienvenue chez nous !</h1>
       <hr />
       <h2>Connectez-vous</h2>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleChange}
-        placeholder="Entrez votre prénom..."
-        required
-      />
+      <div className="input-with-icon">
+        <BsPersonCircle className="icon" />
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Entrez votre prénom..."
+          required
+        />
+      </div>
       <button>Accédez à votre espace</button>
     </LoginFormStyled>
   );
@@ -59,5 +63,32 @@ const LoginFormStyled = styled.form`
     color: white;
     margin: 20px 10px 10px;
     font-size: 36px;
+  }
+
+  .input-with-icon {
+    /* border: 1px solid red; */
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 18px 24px;
+    margin: 18px 0;
+
+    .icon {
+      font-size: 15px;
+      margin-right: 8px;
+      color: #93a2b1;
+    }
+
+    input {
+      border: none;
+      font-size: 15px;
+      color: #17161a;
+    }
+
+    &::placeholder {
+      background: #000;
+      color: lightgrey;
+    }
   }
 `;
