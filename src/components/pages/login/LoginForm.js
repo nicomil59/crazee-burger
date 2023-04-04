67ba5a67f04d7d4f3edb7912cfbfcdf5bb2eb9e2
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-04-04 15:47:21 +0200

    💄 F03: LOGIN > LoginForm > Add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const LoginForm = () => {
-    const [inputValue, setInputValue] = useState("");
-    const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      setInputValue("");
-      navigate(`order/${inputValue}`);
-    };
-  
-    const handleChange = (e) => {
-      setInputValue(e.target.value);
-    };
-  
-    return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputValue("");
+    navigate(`order/${inputValue}`);
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
     <LoginFormStyled onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
-      <br />
+      <hr />
       <h2>Connectez-vous</h2>
       <input
         type="text"
@@ -30,11 +30,34 @@ const LoginForm = () => {
       />
       <button>Accédez à votre espace</button>
     </LoginFormStyled>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
 
 const LoginFormStyled = styled.form`
   background: olive;
+  text-align: center;
+  max-width: 500px;
+  min-width: 400px;
+  margin: 0 auto;
+  padding: 2.5rem 2rem;
+  border-radius: 5px;
+  font-family: "Amatic SC", cursive;
+
+  hr {
+    border: 1.5px solid #f56a2c;
+    margin-bottom: 40px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 48px;
+  }
+
+  h2 {
+    color: white;
+    margin: 20px 10px 10px;
+    font-size: 36px;
+  }
 `;
