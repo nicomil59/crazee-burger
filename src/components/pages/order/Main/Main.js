150481f85423e470ef9af88eeab19acdbef750fa
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-05-24 15:56:13 +0200

    ♻️ F05: ORDER > MAIN > Create Menu

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
+import Menu from "./Menu";
 
 const Main = () => {
   return <MainStyled>
     <div className="basket">Basket</div>
-    <div className="menu">Menu</div>
+    <Menu />
   </MainStyled>;
 };
 
@@ -22,9 +23,5 @@ const MainStyled = styled.div`
 
   .basket {
     background: pink;
-  }
-  
-  .menu {
-    background: purple;
   }
 `;
---
+++ src/components/pages/order/Main/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Menu = () => {
+  return <MenuStyled>Menu</MenuStyled>;
+};
+
+export default Menu;
+
+const MenuStyled = styled.div`
+  background: purple;
+`;
