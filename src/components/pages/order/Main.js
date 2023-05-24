76fbe8a02f1851bb28ed59cec706184ec84f04e7
Nicolas Milhem <nicolasmilhem@gmail.com>
2023-05-24 15:21:51 +0200

    🚧 F05: ORDER > MAIN > Add grid layout

--- src/components/pages/order/Main.js
+++ src/components/pages/order/Main.js
@@ -3,7 +3,10 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 const Main = () => {
-  return <MainStyled></MainStyled>;
+  return <MainStyled>
+    <div className="basket">Basket</div>
+    <div className="menu">Menu</div>
+  </MainStyled>;
 };
 
 export default Main;
@@ -13,4 +16,15 @@ const MainStyled = styled.div`
   flex: 1; // or you can also use this : height: calc(95vh - 10vh);
   border-radius: 0 0 ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  display: grid;
+  grid-template-columns: 25% 1fr;
+
+  .basket {
+    background: pink;
+  }
+  
+  .menu {
+    background: purple;
+  }
 `;
