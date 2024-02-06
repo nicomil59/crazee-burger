49b1754f6c84202e4b3de86a8c18b147d42eee7f
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-02-06 11:43:50 +0100

    ♻️ F07: THEME > Update font-size and shadows

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,14 +10,13 @@ const colors = {
   red: "#e25549",
   redSecondary: "#c4151c",
   blue: "#51a7e1",
-  greyExtraLight: "lightgrey",
   greyLight: "#e4e5e9",
   greyMedium: "#a7a8ad",
   greySemiDark: "#93a2b1",
   greyDark: "#6f737e",
   greyBlue: "#747b91",
-  loginLine: "#f56a2c"
-};
+  loginLine: "#f56a2c",
+}
 
 const spacing = {
   xxs: "4px",
@@ -27,14 +26,15 @@ const spacing = {
   lg: "32px",
   xl: "52px",
   xxl: "84px",
-};
+}
 
 const fonts = {
   size: {
     XXXS: "8px",
     XXS: "10px",
     XS: "12px",
-    P0: "15px",
+    SM: "15px",
+    P0: "16px",
     P1: "18px",
     P2: "20px",
     P3: "24px",
@@ -50,18 +50,20 @@ const fonts = {
     bold: 700,
     heavy: 800,
   },
-};
-
-const gridUnit = 8;
-
+}
+const gridUnit = 8
 const borderRadius = {
   subtle: 1,
   round: "5px",
   extraRound: "15px",
   circle: "50%",
-};
+}
 
-const shadows = {};
+const shadows = {
+  subtle: "0px -6px 8px -2px rgba(0, 0, 0, 0.1)",
+  medium: "-8px 8px 20px 0px rgb(0 0 0 / 20%)",
+  strong: "0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset",
+}
 
 export const theme = {
   colors,
@@ -69,5 +71,5 @@ export const theme = {
   gridUnit,
   borderRadius,
   shadows,
-  spacing
-};
+  spacing,
+}
