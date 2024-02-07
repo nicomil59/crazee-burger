ea92b3bb6e36ce288be8a5169b163b520695f2f6
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-02-07 11:15:31 +0100

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

---
+++ src/components/pages/order/Main/Admin/AdminPanel.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+
+const AdminPanel = () => {
+  return (
+    <AdminPanelStyled>AdminPanel</AdminPanelStyled>
+  )
+}
+
+export default AdminPanel;
+
+const AdminPanelStyled = styled.div`
+  height: 250px;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+`;
---
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const AdminTabs = () => {
+  return (
+    <AdminTabsStyled>AdminTabs</AdminTabsStyled>
+  )
+}
+
+export default AdminTabs;
+
+const AdminTabsStyled = styled.div`
+  border: 1px solid red;
+`;
