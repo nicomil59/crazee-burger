34592d65290c3b1ff9d570858c382cf9b28d9565
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-03-06 17:11:02 +0100

    🚧 F08: ADMIN > AddForm > Add inputs layout

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const AddForm = () => {
   return (
-    <div>AddForm</div>
-  )
-}
+    <AddFormStyled>
+      <div className="image-preview">ImagePreview</div>
+      <div className="input-fields">
+        <input type="text" placeholder="Nom" />
+        <input type="text" placeholder="Image Url" />
+        <input type="text" placeholder="Prix" />
+      </div>
+      <button className="submit-button">Submit button</button>
+    </AddFormStyled>
+  );
+};
 
-export default AddForm
+export default AddForm;
+
+const AddFormStyled = styled.form`
+  border: 2px solid black;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  height: 100%;
+  width: 70%;
+
+  .image-preview {
+    background: red;
+    grid-area: 1 / 1 / 4 / 2;
+  }
+  .input-fields {
+    background: blue;
+    grid-area: 1 / 2 / 4 / 3;
+    display: grid;
+  }
+  .submit-button {
+    background: green;
+    grid-area: 4 / 2 / 5 / 3;
+    width: 50%;
+  }
+`;
