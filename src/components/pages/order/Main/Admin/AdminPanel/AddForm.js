02ffd9f72d6c6ca7b1bffc33fccc3096669fc0ad
Nicolas Milhem <nicolasmilhem@gmail.com>
2024-03-20 15:12:01 +0100

    💄 F08: AddForm > Add submit message style

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.js
@@ -74,34 +74,38 @@ const AddForm = () => {
           name="title"
           type="text"
           placeholder="Nom du produit (ex: Super Burger)"
-          Icon={<FaHamburger/>}
+          Icon={<FaHamburger />}
           version="minimalist"
-          />
+        />
         <TextInput
           onChange={handleChange}
           value={newProduct.imageSource}
           name="imageSource"
           type="text"
           placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
-          Icon={<BsFillCameraFill/>}
+          Icon={<BsFillCameraFill />}
           version="minimalist"
-          />
+        />
         <TextInput
           onChange={handleChange}
           value={newProduct.price ? newProduct.price : ""}
           name="price"
           type="number"
           placeholder="Prix"
-          Icon={<MdOutlineEuro/>}
+          Icon={<MdOutlineEuro />}
           version="minimalist"
         />
       </div>
       <div className="submit">
-        <Button label="Ajoutez un nouveau produit au menu" version="success" className="submit-button" />
+        <Button
+          label="Ajoutez un nouveau produit au menu"
+          version="success"
+          className="submit-button"
+        />
         {isSubmitted && (
           <div className="submit-message">
-            <FiCheck />
-            <span>Ajouté avec succès !</span>
+            <FiCheck className="icon" />
+            <span className="message">Ajouté avec succès !</span>
           </div>
         )}
       </div>
@@ -158,13 +162,35 @@ const AddFormStyled = styled.form`
     grid-area: 4 / 2 / 5 / 3;
     display: flex;
     align-items: center;
+    position: relative;
+    top: 3px;
 
     .submit-button {
       width: 50%;
     }
 
     .submit-message {
-      border: 1px solid red;
+      /* border: 1px solid red; */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 5px;
+
+      .icon {
+        color: ${theme.colors.success};
+        margin-left: 10px;
+        width: 1em;
+        height: 1em;
+        border: 1px solid ${theme.colors.success};
+        border-radius: 50%;
+        vertical-align: middle;
+      }
+
+      .message {
+        margin-left: 5px;
+        font-size: ${theme.fonts.size.SM};
+        color: ${theme.colors.success};
+      }
     }
   }
 `;
